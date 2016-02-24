be6a7907b17dc3ddfde85e689397d80c9e6ebc00
Roman Rybalchenko (nerevar) <nerevar@yandex-team.ru>
2016-02-24 20:16:30 +0300

    release script and changelog

---
+++ sources/release.bat
@@ -0,0 +1,15 @@
+@echo off
+
+SET release=jmc%1
+
+mkdir %release%
+copy jmc.exe %release%
+copy ttcoreex.dll %release%
+copy recore.dll %release%
+copy language.ini %release%
+copy changelog.txt %release%
+copy ttcoreex.bat %release%
+
+"c:\Program Files\7-Zip\7z.exe" a %release%.zip %release%
+
+rmdir /s/q %release%
